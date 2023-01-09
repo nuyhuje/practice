e030181807f1ca87aeb72ce482a1e7e6fc42e86f
nuyhuje <xcvivxiii@gmail.com>
2023-01-09 16:44:10 +0900

    [AP] 설화수 03

--- Sulwhasoo/js/custom.js
+++ Sulwhasoo/js/custom.js
@@ -8,4 +8,20 @@ $(function () {
     duration: 500,
   });
 
+  /* 상단 이동 스크롤 버튼 */
+  $(window).scroll(function(){
+    
+    if($(this).scrollTop() > 100){
+      $(".scroll-top-button").addClass("is-active");
+    }
+    else{
+      $(".scroll-top-button").removeClass("is-active");
+    }
+
+  });
+
+  $(".scroll-top-button").click(function(){
+    window.scrollTo({top: 0, behavior: "smooth"});
+  });
+
 });
