aadc143b36a2be101d65290d437732f3c1a09c74
cttd05 <cttd05@gepplus.co.kr>
2023-02-23 16:13:12 +0900

    [AP] 설화수 04

--- Sulwhasoo/js/custom.js
+++ Sulwhasoo/js/custom.js
@@ -1,27 +1,26 @@
-$(function () {
-  
-  /* 영상 재생 */
-
-
-  /* AOS 모션 */
-  AOS.init({
-    duration: 500,
-  });
-
-  /* 상단 이동 스크롤 버튼 */
-  $(window).scroll(function(){
-    
-    if($(this).scrollTop() > 100){
-      $(".scroll-top-button").addClass("is-active");
-    }
-    else{
-      $(".scroll-top-button").removeClass("is-active");
-    }
-
-  });
-
-  $(".scroll-top-button").click(function(){
-    window.scrollTo({top: 0, behavior: "smooth"});
-  });
-
+/* AOS 모션 */
+AOS.init({
+  duration: 500
 });
+
+/* 상단 이동 스크롤 버튼 */
+$(window).scroll(function(){
+  if($(this).scrollTop() > 200){
+    $(".scroll-top-button").addClass("is-active");
+  }
+  else{
+    $(".scroll-top-button").removeClass("is-active");
+  }
+});
+
+$(".scroll-top-button").click(function(){
+  window.scrollTo({top: 0, behavior: "smooth"});
+});
+
+/* 영상 재생 */
+function video_play(button, frame) {
+  $('#' + button).hide();
+  $('#' + frame)[0].src += "?autoplay=1&mute=1";
+};
+// $('.promotion__video--button').hide();
+// $('#youtube-player')[0].src += "?autoplay=1&mute=1";
